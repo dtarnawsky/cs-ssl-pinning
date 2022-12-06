6e6146353e946f5c3116529c247cf78b366177be
Damian Tarnawsky <damian@ionic.io>
2022-12-06 06:19:59 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.ssl.pinning',
+  appName: 'ionic-ssl-pinning',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
